f7e19482919ede00c7ba325be7e2d4dd5546063a
Jessica Ortiz <70552512+JessicaOI@users.noreply.github.com>
2023-10-20 20:22:45 -0600

    Forecasting

---
+++ Forecasting/README.py
@@ -0,0 +1,7 @@
+Task8_9_1 : El primer modelo creado, el que se llama my_model.keras
+Task8_9_2 : El modelo "dropout4" de la carpeta model_directory(cambio en hiperparametros)
+Task8_9_3 : El modelo "dropout6" de la carpeta model directory(cambio en hiperparametros)
+
+Los mejores pronósticos se obtuvieron en el Task8_9_1, ya que la gráfica muestra cómo habrá buenas ventas al principio del intervalo de 3 meses y luego se mantiene por encima de 0 durante un buen tiempo.
+
+En cambio, en las otras dos gráficas, los valores se mantienen en 0 o por debajo de este, lo que no es un buen indicio
